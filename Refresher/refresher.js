a40dba64d0debc7020f477a6ef31998fadb2523a
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-09-22 12:18:00 +0100

    Added JS modules

--- Refresher/refresher.js
+++ Refresher/refresher.js
@@ -14,7 +14,7 @@ const sandwich = {
     toppings: ["lettuce", "tomato", "mustard"]
 };
 
-let {bread, meat} = sandwich;
+let { bread, meat } = sandwich;
 
 bread = "garlic";
 meat = "turkey";
@@ -58,7 +58,7 @@ const regularPerson1 = {
 }
 
 //Lordify the spouse's first name
-const lordify1 =({spouse: { firstName }}) => {
+const lordify1 = ({ spouse: { firstName } }) => {
     console.log(`${firstName} of Canterbury`)
 }
 
@@ -77,18 +77,18 @@ console.log(thirdAnimal); //Cat
 const name1 = "Tallac";
 const elevation = 9738;
 
-const funHike = { name1, elevation}
+const funHike = { name1, elevation }
 console.log(funHike); // {name: "Tallac", elevation: 9738}
 
 // function restructuring
 const name2 = "Tallac";
 const elevation1 = 9738;
-const print = function() {
+const print = function () {
     console.log(`Mt. ${this.name2} is ${this.elevation1} feet tall`)
 };
 
 
-const funHike1 = { name2, elevation1, print}
+const funHike1 = { name2, elevation1, print }
 console.log(funHike); // {name: "Tallac", elevation: 9738}
 
 // The spread Operator
@@ -118,3 +118,74 @@ const [first, ...others] = lakes;
 
 console.log(others.join(", ")) // Marlette, Fallen Leaf, Cascade
 
+
+//Simple Promises with Fetch
+console.log(fetch("https://api.randomuser.me/?nat=US&results=1"))
+
+fetch("https://api.randomuser.me/?nat=US&results=1").then(res =>
+    console.log(res.json))
+    .then(json.results)
+    .then(console.log)
+    .catch(console.error)
+
+// Making an API call with async function
+const getFakePerson = async () => {
+    let res = await fetch("https://api.randomuser.me/?nat=US&results=1");
+    let { results } = res.json();
+    console.log(results);
+}
+
+getFakePerson();
+
+const getAnotherPerson = async () => {
+    try {
+        let res = await fetch ("https://api.randomuser.me/?nat=US&results=1");
+        let { results } = res.json();
+        console.log(results);
+    }catch (error) {
+        console.error(error)
+    }
+}
+getAnotherPerson();
+
+//ClASSES
+class Vacation {
+    constructor(destination, length) {
+
+        this.destination = destination;
+        this.length = length;
+    }
+
+    print() {
+        console.log(`${this.destination} will take ${this.length} days`)
+    }
+}
+
+const trip = new Vacation("Santiago, Chile", 7);
+trip.print(); // Chile will take 7 days
+
+// INHERITANCE
+class Expendition extends Vacation {
+    constructor(destination, length, gear) {
+        super(destination, length)
+        this.gear = gear;
+    }
+
+    print() {
+        super.print();
+        console.log(`Bring your ${this.gear.join(" and your ")}`)
+    }
+}
+
+const trip2 = new Expendition("Mt. Whitney", 3, [
+    "sunglasses",
+    "prayer flags",
+    "camera"
+])
+
+trip2.print();
+
+//MODULES
+export const print3 =(message) => log(message, new Date())
+
+export const log=(message, timestamp) => console.log(`${timestamp.toString()}: ${message}`)
